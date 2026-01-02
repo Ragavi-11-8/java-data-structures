e27b291a06e48a1c39191f0d0c078d8de3a86170
Ragavi Poornima <poornimaragavi@gmail.com>
2026-01-02 23:19:44 +0530

    first commit

---
+++ src/BigO.java
@@ -0,0 +1,17 @@
+
+public class BigO {
+    public static void printItems(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println(i +  " "  + j);
+            }
+        }
+        for (int k = 0; k < n; k++) {
+            System.out.println(k);
+        }
+    }
+
+    public static void main(String[] args) {
+        printItems( 10) ;
+    }
+}
---
+++ src/Classes/Main.java
@@ -0,0 +1,35 @@
+package Classes;
+
+public class Main{
+    public static void main(String[] args) {
+        LinkedList linkedlist=new LinkedList(1);
+
+        linkedlist.append(2);
+        linkedlist.append(4);
+
+        // (2) Items - Returns 2 Node
+        System.out.println(linkedlist.removeLast().value);
+        // (1) Item - Returns 1 Node
+        System.out.println(linkedlist.removeLast().value);
+        // (0) Items - Returns null
+        System.out.println(linkedlist.removeLast());
+
+
+//        linkedlist.getHead();
+//        linkedlist.getTail();
+//        linkedlist.getLength();
+//        System.out.println("\nLinked List:");
+//        linkedlist.printList();
+    }
+}
+//public class Main {
+//    public static void main(String[] args) {
+//        Cookie cookieOne= new Cookie("green");
+//        Cookie cookieTwo= new Cookie("blue");
+//
+//        cookieOne.setColor("yellow");
+//        System.out.println(cookieOne.getColor());
+//        System.out.println(cookieTwo.getColor());
+//    }
+//}
+
