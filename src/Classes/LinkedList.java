3fd3e53f448aaa4b659194d7382f74fa994e1670
Ragavi Poornima <poornimaragavi@gmail.com>
2026-01-03 22:59:52 +0530

    get,set methods

--- src/Classes/LinkedList.java
+++ src/Classes/LinkedList.java
@@ -106,6 +106,24 @@ class Node {
         return temp;
     }
 
+    public Node get(int index) {
+        if (index < 0 || index >= length) return null;
+        Node temp = head;
+        for(int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if (temp != null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+
 
 
 }
--- src/Classes/Main.java
+++ src/Classes/Main.java
@@ -2,19 +2,31 @@ package Classes;
 
 public class Main{
     public static void main(String[] args) {
-        LinkedList myLinkedList=new LinkedList(2);
+        LinkedList myLinkedList=new LinkedList(0);
 
         // (2) Items - Returns 2 NodemyLink
 
+        myLinkedList.append(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
 
-        myLinkedList.append(7);
+        System.out.println("Linked List before set():");
+        myLinkedList.printList();
 
-        // (2) Items - Returns 2 Node
-        System.out.println(myLinkedList.removeFirst().value);
-        // (1) Item - Returns 1 Node
-        System.out.println(myLinkedList.removeFirst().value);
-        // (0) Items - Returns null
-        System.out.println(myLinkedList.removeFirst());
+        myLinkedList.set(2, 99);
+
+        System.out.println("\nLinked List after set():");
+        myLinkedList.printList();
+
+
+//        myLinkedList.append(7);
+//
+//        // (2) Items - Returns 2 Node
+//        System.out.println(myLinkedList.removeFirst().value);
+//        // (1) Item - Returns 1 Node
+//        System.out.println(myLinkedList.removeFirst().value);
+//        // (0) Items - Returns null
+//        System.out.println(myLinkedList.removeFirst());
 
 
 
